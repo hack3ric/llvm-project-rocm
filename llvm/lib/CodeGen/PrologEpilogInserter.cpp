al42and/RadeonOpenCompute-llvm-project

[HeterogeneousDWARF] Implement PrologEpilogInserter
Add a target-overridable hook to frame lowering called
insertFrameLocation, which must insert an expression which describes the
base of the current frame. This permits the generic PEI implementation
to offset the base expression to create the final expression for the
stack slot.

Change-Id: Idcd1ff66255a2c4c4f9dccc42e8483860ac25469

llvm/lib/CodeGen/PrologEpilogInserter.cpp
@@ -1325,6 +1325,8 @@ void PEI::replaceFrameIndices(MachineBasicBlock *BB, MachineFunction &MF,
   const TargetInstrInfo &TII = *MF.getSubtarget().getInstrInfo();
   const TargetRegisterInfo &TRI = *MF.getSubtarget().getRegisterInfo();
   const TargetFrameLowering *TFI = MF.getSubtarget().getFrameLowering();
+  const DataLayout &DL = MF.getDataLayout();
+  LLVMContext &Context = MF.getMMI().getModule()->getContext();
 
   if (RS && FrameIndexEliminationScavenging)
     RS->enterBasicBlock(*BB);
@@ -1345,6 +1347,45 @@ void PEI::replaceFrameIndices(MachineBasicBlock *BB, MachineFunction &MF,
     for (unsigned i = 0, e = MI.getNumOperands(); i != e; ++i) {
       if (!MI.getOperand(i).isFI())
         continue;
+
+      if (MI.isDebugDef()) {
+        MachineOperand &Op = MI.getOperand(i);
+        assert(MI.isDebugOperand(&Op) &&
+               "Frame indices can only appear as a debug operand in a DBG_DEF"
+               " machine instruction");
+        assert(&Op == &MI.getDebugReferrer() &&
+               "Frame indices can only appear as the referrer of DBG_DEF "
+               "machine instructions");
+        Register Reg;
+        unsigned FrameIdx = Op.getIndex();
+        StackOffset Offset = TFI->getFrameIndexReference(MF, FrameIdx, Reg);
+
+        if (Reg) {
+          Op.ChangeToRegister(Reg, false /*isDef*/);
+          Op.setIsDebug();
+        } else {
+          Op.ChangeToImmediate(0);
+        }
+
+        DILifetime *Lifetime = MI.getDebugLifetime();
+        DIExprBuilder Builder = Lifetime->getLocation()->builder();
+        for (auto &&I = Builder.begin(); I != Builder.end(); ++I) {
+          if (auto *Referrer = I->getIf<DIOp::Referrer>()) {
+            Type *ResultType = Referrer->getResultType();
+            unsigned PointerSizeInBits =
+                DL.getPointerSizeInBits(DL.getAllocaAddrSpace());
+            ConstantData *C =
+                ConstantInt::get(IntegerType::get(Context, PointerSizeInBits),
+                                 Offset.getFixed(), true);
+            std::initializer_list<DIOp::Variant> IL = {
+                DIOp::Constant(C), DIOp::ByteOffset(ResultType)};
+            I = TFI->insertFrameLocation(
+                MF, Builder, Builder.insert(Builder.erase(I), IL), ResultType);
+          }
+        }
+        Lifetime->setLocation(Builder.intoExpr());
+        continue;
+      }
 
       // Frame indices in debug values are encoded in a target independent
       // way with simply the frame index and offset rather than any

hack3ric/llvm-project-rocm

llvm/lib/CodeGen/PrologEpilogInserter.cpp
@@ -1395,8 +1395,49 @@ bool PEI::replaceFrameIndexDebugInstr(MachineFunction &MF, MachineInstr &MI,
                                       unsigned OpIdx, int SPAdj) {
   const TargetFrameLowering *TFI = MF.getSubtarget().getFrameLowering();
   const TargetRegisterInfo &TRI = *MF.getSubtarget().getRegisterInfo();
-  if (MI.isDebugValue()) {
+  const DataLayout &DL = MF.getDataLayout();
+  LLVMContext &Context = MF.getMMI().getModule()->getContext();
 
+  if (MI.isDebugDef()) {
+    MachineOperand &Op = MI.getOperand(OpIdx);
+    assert(MI.isDebugOperand(&Op) &&
+           "Frame indices can only appear as a debug operand in a DBG_DEF"
+           " machine instruction");
+    assert(&Op == &MI.getDebugReferrer() &&
+           "Frame indices can only appear as the referrer of DBG_DEF "
+           "machine instructions");
+    Register Reg;
+    unsigned FrameIdx = Op.getIndex();
+    StackOffset Offset = TFI->getFrameIndexReference(MF, FrameIdx, Reg);
+
+    if (Reg) {
+      Op.ChangeToRegister(Reg, false /*isDef*/);
+      Op.setIsDebug();
+    } else {
+      Op.ChangeToImmediate(0);
+    }
+
+    DILifetime *Lifetime = MI.getDebugLifetime();
+    DIExprBuilder Builder = Lifetime->getLocation()->builder();
+    for (auto &&I = Builder.begin(); I != Builder.end(); ++I) {
+      if (auto *Referrer = std::get_if<DIOp::Referrer>(&*I)) {
+        Type *ResultType = Referrer->getResultType();
+        unsigned PointerSizeInBits =
+            DL.getPointerSizeInBits(DL.getAllocaAddrSpace());
+        ConstantData *C =
+            ConstantInt::get(IntegerType::get(Context, PointerSizeInBits),
+                             Offset.getFixed(), true);
+        std::initializer_list<DIOp::Variant> IL = {
+            DIOp::Constant(C), DIOp::ByteOffset(ResultType)};
+        I = TFI->insertFrameLocation(
+            MF, Builder, Builder.insert(Builder.erase(I), IL), ResultType);
+      }
+    }
+    Lifetime->setLocation(Builder.intoExpr());
+    return true;
+  }
+
+  if (MI.isDebugValue()) {
     MachineOperand &Op = MI.getOperand(OpIdx);
     assert(MI.isDebugOperand(&Op) &&
            "Frame indices can only appear as a debug operand in a DBG_VALUE*"